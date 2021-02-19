84adb54d80d322fa6967528ea51aa3043be39ad3
Evgeny Vasilev <jony.torrent@gmail.com>
2021-02-19 19:26:35 +0300

    test fix

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -17,17 +21,18 @@ public class CardDeliveryOrderTest {
     void shouldSucсessOrder() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar today = Calendar.getInstance();
-        today.add(Calendar.DAY_OF_MONTH,4);
-        String visitDate=(String)(dateFormat.format(today.getTime()));
+        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"A"+Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(visitDate);
+        $("[data-test-id=date] input").setValue(date);
         $("[data-test-id=name] input").setValue("Васильев Евгений");
         $("[data-test-id=phone] input").setValue("+79231765501");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно!")).waitUntil(visible,15000);
+        $(" [data-test-id=notification]").shouldBe(visible, Duration.ofMillis(15000))
+                .shouldHave(exactText("Успешно! Встреча успешно забронирована на " + date));
+
+        
     }
 
 
